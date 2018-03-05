d7a53a454e552650eb4483439da6a8ec54ce852a
Alex M <RobotXLabs@users.noreply.github.com>
2018-03-05 11:18:30 -0500

    Update and rename overtone.py to overtonic.py

--- overtone.py
+++
@@ -1,1 +0,0 @@
-#Using neural networks to determine an instrument based on the musical overtones of its sound
---
+++ overtonic.py
@@ -0,0 +1,2 @@
+# overtonic.py
+# Using neural networks to determine an instrument based on the musical overtones of its sound

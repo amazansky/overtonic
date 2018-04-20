5f0c8550712472ce1a176a27e6a6d5e078647f6b
RobotXLabs <databasemaster9233@gmail.com>
2018-04-20 06:59:45 -0400

    Fast Fourier Transform
    - Add to .gitignore
    - Remove data.md
    - Rework overtonic.py for FFTs
    - Add to README.md
    - Add four sample sounds that will be used with the project

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # Overtonic
-Overtonic is a project that aims to use neural networks to determine the source of a sound based on its overtones. For Science Fair 2018.
+Overtonic is a project that aims to use Fast Fourier Transforms (FFTs) to determine the source of a sound based on its overtones. (:musical_note: Science Fair 2018)
 
-## Data
-The data collected for this project is available in data.md and quantified in the main (overtonic.py) file.
+```
+computer:overtonic user$ python3 overtonic.py
+Reading from sound/sine-a4.wav
+FRATE Variable: 11025
+DATA Variable: [    0  7940 15384 ... 31997 30888 27848]
+Indexes [1596]
+FreqArray of Indexes [439.8975]
+It looks like the sound you gave me is a SINE WAVE
+```
 
 ## Dependencies
 1. NumPy
+1. SciPy
+  * scipy.fftpack
+  * scipy.io
+1. PyLab
+1. MatPlotLib
+  * matplotlib.pyplot
+1. PeakUtils
 
 ## Acknowledgements
-* Base neural network code: [miloharper/simple-neural-network](https://github.com/miloharper/simple-neural-network) _(Licensed under MIT by Milo Spencer-Harper, 2015) *Updated for Python 3*_
+* FFT Identification code: [Basic Sound Processing with Python](http://samcarcagno.altervista.org/blog/basic-sound-processing-python/) _(Sam Carcagno, 2013) **Modified to acommodate frequency and amplitude**_
+* PeakUtils: [PeakUtils tutorial (docs)](https://peakutils.readthedocs.io/en/latest/tutorial_a.html)
+* With help from [thatoddmailbox](https://github.com/thatoddmailbox/)
--- overtonic.py
+++ overtonic.py
@@ -1,102 +1,85 @@
 # overtonic.py
-# Using neural networks to determine an instrument based on the musical overtones of its sound
-#
-#
-# This code is based on https://github.com/miloharper/
-# simple-neural-network which is licensed under MIT.
-#
-# The MIT License (MIT)
-# Copyright (c) 2015 Milo Spencer-Harper
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# Using Fast Fourier Transforms (FFTs) to determine an instrument based on the musical overtones of its sound
+
+import numpy as np
+from scipy.fftpack import fft
+from scipy.io import wavfile as wav
+from pylab import *
+import matplotlib.pyplot as plt
+import peakutils.plot
+
+##################################################
+
+# Program variables
+
+# The sound file you want to read from
+# If you change this, remember to change the savefig location to match your directory tree!
+soundfile = 'sound/sine-a4.wav'
+
+# Set to True if you want the output figure (with peaks) to save!
+# Usually best to leave at false except for demos, etc.
+# Split is a bit questionable, figure out how to do it the right way
+# This will need to be changed if a different directory tree is used
+savefig = False
+savefigloc = 'images/' + soundfile.split('/')[1] + '.png'
 
 
-from numpy import exp, array, random, dot
-class NeuralNetwork():
-    def __init__(self):
-        # Seed the random number generator, so it generates the same numbers
-        # every time the program runs.
-        random.seed(1)
+##################################################
 
-        # We model a single neuron, with 3 input connections and 1 output connection.
-        # We assign random weights to a 3 x 1 matrix, with values in the range -1 to 1
-        # and mean 0.
-        self.synaptic_weights = 2 * random.random((3, 1)) - 1
+# Print helpful data
+print("Reading from", soundfile)
+frate, data = wav.read(soundfile)
+print("FRATE:", frate)
+print("DATA:", data)
 
-    # The Sigmoid function, which describes an S shaped curve.
-    # We pass the weighted sum of the inputs through this function to
-    # normalise them between 0 and 1.
-    def __sigmoid(self, x):
-        return 1 / (1 + exp(-x))
+length = len(data)
+nUniquePts = int(ceil((length+1)/2.0))
+fft = (abs(fft(data)[0:nUniquePts]) / float(length))**2 
 
-    # The derivative of the Sigmoid function.
-    # This is the gradient of the Sigmoid curve.
-    # It indicates how confident we are about the existing weight.
-    def __sigmoid_derivative(self, x):
-        return x * (1 - x)
+if length % 2 > 0: # odd, double the fft set
+   fft[1:len(fft)] = fft[1:len(fft)] * 2
+else: # even, double the fft set except for Nyquist
+   fft[1:len(fft) -1] = fft[1:len(fft) - 1] * 2 
 
-    # We train the neural network through a process of trial and error.
-    # Adjusting the synaptic weights each time.
-    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-        for iteration in range(number_of_training_iterations):
-            # Pass the training set through our neural network (a single neuron).
-            output = self.think(training_set_inputs)
+array = arange(0, nUniquePts, 1.0) * (frate / length)
+x = array[:4000] # cap output to 4000 Hz
 
-            # Calculate the error (The difference between the desired output
-            # and the predicted output).
-            error = training_set_outputs - output
+y = (10*log10(fft))[:4000] # cap output to 4000 Hz
 
-            # Multiply the error by the input and again by the gradient of the Sigmoid curve.
-            # This means less confident weights are adjusted more.
-            # This means inputs, which are zero, do not cause changes to the weights.
-            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+y2 = y + np.polyval([0.002,-0.08,5], x)
+base = peakutils.baseline(y2, 2) # remove the baseline for a cleaner reading
 
-            # Adjust the weights.
-            self.synaptic_weights += adjustment
+# TODO: Conditional-ize the thres and min_dist per sound if possible
+indexes = peakutils.indexes(y, thres=0.7, min_dist=100)
 
-    # The neural network thinks.
-    def think(self, inputs):
-        # Pass inputs through our neural network (our single neuron).
-        return self.__sigmoid(dot(inputs, self.synaptic_weights))
+print("Indexes", indexes)
+print("array of Indexes", array[indexes])
 
+peakutils.plot.plot(x, y2-base, indexes) # plot x and y with removed baseline, peaks (indexes)
+plt.title('Peaks after Removed Baseline')
+xlabel('Frequency (Hz)')
+ylabel('Amplitude (dB) without Baseline')
 
-if __name__ == "__main__":
+# Save figure, use for demo only
+if savefig == True:
+    plt.savefig(savefigloc, bbox_inches='tight')
 
-    #Intialise a single neuron neural network.
-    neural_network = NeuralNetwork()
+plt.show()
 
-    print("Random starting synaptic weights: ")
-    print(neural_network.synaptic_weights)
+# TODO:
+	# Set the conditions for the if/else block
+    # Add a "more or less" part to ensure that this works with other files of the same sound source
 
-    # The training set. We have 4 examples, each consisting of 3 input values
-    # and 1 output value.
-    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
-    training_set_outputs = array([[0, 1, 1, 0]]).T
+inst = None
+if len(indexes) == 1: # Sine waves have one peak
+    inst = 'a SINE WAVE'
+elif len(indexes) == 2: # Whistles have two peaks
+    inst = 'a WHISTLE'
+elif len(indexes) == 5: # Pianos have five peaks, but not really and this should be changed! (TODO)
+    inst = 'a PIANO'
 
-
-    # Train the neural network using a training set.
-    # Do it 10,000 times and make small adjustments each time.
-    neural_network.train(training_set_inputs, training_set_outputs, 10000)
-
-    print("New synaptic weights after training: ")
-    print(neural_network.synaptic_weights)
-
-    # Test the neural network with a new situation.
-    print("Considering new situation [1, 0, 0] -> ?: ")
-print(neural_network.think(array([1, 0, 0])))
+if inst:
+    print('It looks like the sound you gave me is', inst)
+else:
+    print("Aww, the sound you gave me is unsure or not supported")
+print()
